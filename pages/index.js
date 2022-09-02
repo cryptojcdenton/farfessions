2610d4ebef40a8d07d7694c659e04f5fed4fef8d
nicoduclare <nico@beb.xyz>
2022-09-02 00:18:48 -0700

    deleted undefined style

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>BEB</title>
         <meta name="description" content="Anon feedback app" />
